8c20769f9472fcacb039bebe3df94c030c8ec0f6
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-11-14 19:56:30 +0100

    Implement simple react counter

--- react/react-widget/src/widget.tsx
+++ react/react-widget/src/widget.tsx
@@ -1,16 +1,28 @@
 import { ReactWidget } from "@jupyterlab/apputils";
 
-import * as React from 'react';
+import React, { useState }  from 'react';
+
+const REACT_WIDGET_CLASS = 'jp-ReactWidget';
 
 const CounterComponent = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
-      TODO
+      <p>You clicked {counter} times!</p>
+      <button onClick={() => { setCounter(counter + 1) }}>
+        Increment
+      </button>
     </div>
   )
 }
 
 export class CounterWidget extends ReactWidget {
+  constructor() {
+    super();
+    this.addClass(REACT_WIDGET_CLASS);
+  }
+
   render() {
     return <CounterComponent />;
   }
