c73c028a3273c0445a4deb8fcd15bc37e97e1c9d
Frederic Collonval <frederic.collonval@safrangroup.com>
2019-11-25 09:04:52 -0500

    Add the code example

--- state/src/index.ts
+++ state/src/index.ts
@@ -3,14 +3,54 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { InputDialog } from '@jupyterlab/apputils';
+
+import { IStateDB } from '@jupyterlab/coreutils';
+
+import { ReadonlyJSONObject } from '@phosphor/coreutils';
+
+const PLUGIN_ID = 'state';
+
 /**
  * Initialization data for the state extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'state',
+  id: PLUGIN_ID,
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension state is activated!');
+  requires: [IStateDB],
+  activate: (app: JupyterFrontEnd, state: IStateDB) => {
+    let choice = 'one';
+
+    app.restored
+      // Get the state object
+      .then(() => state.fetch(PLUGIN_ID))
+      .then(value => {
+        // Get the choice attribute
+        if (value) {
+          choice = (value as ReadonlyJSONObject)['choice'] as string;
+        }
+
+        // Ask the user to pick a choice with `choice` as default
+        return InputDialog.getItem({
+          title: 'Pick a choice',
+          items: ['one', 'two', 'three'],
+          current: choice
+        });
+      })
+      .then(result => {
+        // If the user click on the accept button of the dialog
+        if (result.button.accept) {
+          // Get the user choice
+          choice = result.value;
+          // Save the choice in the state database
+          return state.save(PLUGIN_ID, { choice });
+        }
+      })
+      .catch(reason => {
+        console.error(
+          `Something went wrong when reading the state for ${PLUGIN_ID}.\n${reason}`
+        );
+      });
   }
 };
 
