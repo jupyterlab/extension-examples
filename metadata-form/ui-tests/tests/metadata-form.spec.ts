a1507862d0ecab0da811176efd8c3cb44f8ab307
Nicolas Brichet <32258950+brichet@users.noreply.github.com>
2023-04-27 17:50:40 +0200

    Add an example package for the use of metadata-form (#208)
    * Add an example package for the use of meatadata-form
    
    * Fix the activate function
    
    * Updates usage of formRegistry, and fix (again) activate function
    
    * Follow metadataform-extension update
    
    * Update schema to match the new one from metadata-form extension
    
    * Update to the latest version of metadataForm
    
    * Adds the metadata-form in test build
    
    * Add a custom field
    
    * Adaptation to the reduced formContext
    
    * Update to JupyterLab v4.0.0a33
    
    * Update example using cookiecutter
    
    * Fix lint and file comparison
    
    * Adds readme
    
    * Adds ui-tests
    
    ---------
    
    Co-authored-by: foo <foo@bar.com>

---
+++ metadata-form/ui-tests/tests/metadata-form.spec.ts
@@ -0,0 +1,69 @@
+import { expect, test, IJupyterLabPageFixture } from '@jupyterlab/galata';
+
+/**
+ * Activate notebook tools side bar.
+ */
+async function activatePropertyInspector(page: IJupyterLabPageFixture) {
+  if ((await page.locator('.jp-NotebookTools').count()) > 0) {
+    if (await page.locator('.jp-NotebookTools').isVisible()) {
+      return;
+    }
+  }
+
+  const widgetButton = page.locator(
+    ".lm-TabBar-tab[title='Property Inspector']"
+  );
+  const buttonPosition = await widgetButton.boundingBox();
+
+  if (buttonPosition === null)
+    throw new Error(
+      'Cannot get the position of the property inspector button.'
+    );
+
+  await page.mouse.click(
+    buttonPosition.x + buttonPosition.width / 2,
+    buttonPosition.y + buttonPosition.height / 2
+  );
+
+  await expect(page.locator('.jp-NotebookTools')).toBeVisible();
+}
+
+test('should create the two example forms', async ({ page }) => {
+  page.notebook.createNew('metadata-form');
+
+  await activatePropertyInspector(page);
+
+  // Retrieves the forms from its header's text, it should be collapsed.
+  const simpleForm = page.locator('.jp-NotebookTools .jp-Collapse', {
+    hasText: 'simple example'
+  });
+  const advancedForm = page.locator('.jp-NotebookTools .jp-Collapse', {
+    hasText: 'advanced example'
+  });
+
+  // Expand the simple example form.
+  await simpleForm.click();
+
+  // Get the formGroup (form content).
+  const simpleFormGroup = simpleForm.locator(
+    '.jp-Collapse-contents .jp-MetadataForm fieldset > .form-group'
+  );
+
+  await expect(simpleFormGroup).toHaveCount(2);
+  expect(await simpleForm.screenshot()).toMatchSnapshot('simple-form.png');
+
+  // Collapse the simple example form.
+  await simpleForm.click();
+
+  // Expand the simple example form.
+  await advancedForm.click();
+
+  // Get the formGroup (form content).
+  const advancedFormGroup = advancedForm.locator(
+    '.jp-Collapse-contents .jp-MetadataForm fieldset > .form-group'
+  );
+
+  // Should display 6 fields (the conditional should be there)
+  await expect(advancedFormGroup).toHaveCount(6);
+  expect(await advancedForm.screenshot()).toMatchSnapshot('advanced-form.png');
+});
