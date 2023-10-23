81a20f5d7ccb28712b5a629c4a0b3a0b0feaca00
Eric Gentry <ericsnekbytes@gmail.com>
2023-10-23 15:48:18 -0400

    Added a basic test.

---
+++ shout-button-message/ui-tests/tests/jupyterlab_shout_button_message.spec.ts
@@ -0,0 +1,6 @@
+
+import { test, expect } from '@jupyterlab/galata';
+
+test('should add a shout button', async ({ page }) => {
+  await expect(page.locator('.jp-shout-button')).toHaveText('Press to Shout');
+});
