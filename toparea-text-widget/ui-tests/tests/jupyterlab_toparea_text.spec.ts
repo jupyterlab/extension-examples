6a42df9bf69f096d2cdeae7fdac246e6f586031b
Eric Gentry <ericsnekbytes@gmail.com>
2023-10-23 14:53:33 -0400

    Added basic playwright test.

---
+++ toparea-text-widget/ui-tests/tests/jupyterlab_toparea_text.spec.ts
@@ -0,0 +1,6 @@
+
+import { test, expect } from '@jupyterlab/galata';
+
+test('should add a top area text', async ({ page }) => {
+  await expect(page.locator('.jp-TopAreaText')).toHaveText('Hello World');
+});
