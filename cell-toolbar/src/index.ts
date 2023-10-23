fd0293c7be2d2426cdb0dde17918aac91a5fc9f5
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-10-23 13:16:29 +0200

    Add an example for custom cell toolbar button

---
+++ cell-toolbar/src/index.ts
@@ -0,0 +1,46 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { INotebookTracker } from '@jupyterlab/notebook';
+import { markdownIcon, runIcon } from '@jupyterlab/ui-components';
+
+const CommandIds = {
+  renderMarkdownCell: 'toolbar-button:render-markdown-cell',
+  runCodeCell: 'toolbar-button:run-code-cell'
+}
+
+/**
+ * Initialization data for the @jupyterlab-examples/cell-toolbar extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@jupyterlab-examples/cell-toolbar:plugin',
+  description: 'A JupyterLab extension to add cell toolbar buttons.',
+  autoStart: true,
+  requires: [INotebookTracker],
+  activate: (app: JupyterFrontEnd, tracker: INotebookTracker) => {
+    const { commands }= app;
+
+    /* Adds a command enabled only on code cell */
+    commands.addCommand(CommandIds.runCodeCell, {
+      icon: runIcon,
+      caption: 'Run a code cell',
+      execute: () => {
+        commands.execute('notebook:run-cell');
+      },
+      isVisible: () => tracker.activeCell?.model.type === 'code'
+    });
+
+    /* Adds a command enabled only on markdown cell */
+    commands.addCommand(CommandIds.renderMarkdownCell, {
+      icon: markdownIcon,
+      caption: 'Render a markdown cell',
+      execute: () => {
+        commands.execute('notebook:run-cell');
+      },
+      isVisible: () => tracker.activeCell?.model.type === 'markdown'
+    });
+  }
+};
+
+export default plugin;
